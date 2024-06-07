4bf23a8b90cde02f5dd6bf4f6cebdc7efc1865d9
Vanasisb <vanasisb@gmail.com>
2024-06-07 23:16:52 +0330

    Add project files.

---
+++ Bookify.Domain/Apartments/Apartment.cs
@@ -0,0 +1,27 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public sealed class Apartment
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+
+    // Address
+    public string Country { get; private set; }
+    public string State { get; private set; }
+    public string ZipCode { get; private set; }
+    public string City { get; private set; }
+    public string Street { get; private set; }
+
+    // Price
+    public decimal PriceAmount { get; private set; }
+    public string PriceCurrency { get; private set; }
+
+    // Cleaning Fee
+    public decimal CleaningFeeAmount { get; private set; }
+    public string CleaningFeeCurrency { get; private set; }
+
+    public DateTime? LastBookedOnUtc { get; private set; }
+
+    public List<Amenity> Amenities { get; private set; } = new();
+}
