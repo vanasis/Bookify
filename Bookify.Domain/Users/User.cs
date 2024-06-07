9bd4fefc0cce2a94e5ed2adf461fd877710eb908
Vanasisb <vanasisb@gmail.com>
2024-06-08 00:52:35 +0330

    Convert anemic User model to rich model

--- Bookify.Domain/Users/User.cs
+++ Bookify.Domain/Users/User.cs
@@ -2,13 +2,16 @@
 
 namespace Bookify.Domain.Users;
 
-public class User: Entity
+public class User : Entity
 {
-    public string FirstName { get; private set; }
-    public string LastName { get; private set; }
-    public string Email { get; private set; }
+    public FirstName FirstName { get; private set; }
+    public LastName LastName { get; private set; }
+    public Email Email { get; private set; }
 
-    public User(Guid id) : base(id)
+    public User(Guid id, FirstName firstName, LastName lastName, Email email) : base(id)
     {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
     }
 }
