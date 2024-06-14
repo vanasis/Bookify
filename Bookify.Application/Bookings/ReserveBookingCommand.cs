9f8da281c7390caaa086ff1bc5e0284b69635104
Vanasisb <vanasisb@gmail.com>
2024-06-14 15:13:51 +0330

    Add ReserveBooking command and command handler

---
+++ Bookify.Application/Bookings/ReserveBookingCommand.cs
@@ -0,0 +1,10 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+
+namespace Bookify.Application.Bookings;
+
+public record ReserveBookingCommand(
+    Guid ApartmentId,
+    Guid UserId,
+    DateOnly StartDate,
+    DateOnly EndDate
+) : ICommand<Guid>;
