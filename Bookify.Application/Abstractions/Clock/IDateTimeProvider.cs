d7511bb7092a064c88f0031a4dec910dcdf89a00
Vanasisb <vanasisb@gmail.com>
2024-06-16 00:11:43 +0330

    Add BookingReservedDomainEventHandler

--- Bookify.Application/Abstractions/Clock/IDateTimeProvider.cs
+++ Bookify.Application/Abstractions/Clock/IDateTimeProvider.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Bookify.Application.Abstractions.Clock;
 
-namespace Bookify.Application.Abstractions.Clock
+public interface IDateTimeProvider
 {
-    internal interface IDateTimeProvider
-    {
-    }
+    DateTime UtcNow { get; }
 }
---
+++ Bookify.Application/Bookings/BookingReservedDomainEventHandler.cs
@@ -0,0 +1,38 @@
+﻿using Bookify.Application.Abstractions.Email;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Bookings.Events;
+using Bookify.Domain.Users;
+using MediatR;
+
+namespace Bookify.Application.Bookings;
+
+public class BookingReservedDomainEventHandler : INotificationHandler<BookingReservedDomainEvent>
+{
+    private readonly IBookingRepository _bookingRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly IEmailService _emailService;
+
+    public BookingReservedDomainEventHandler(IBookingRepository bookingRepository, IUserRepository userRepository, IEmailService emailService)
+    {
+        _bookingRepository = bookingRepository;
+        _userRepository = userRepository;
+        _emailService = emailService;
+    }
+
+    public async Task Handle(BookingReservedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var booking = await _bookingRepository.GetByIdAsync(notification.BookingId, cancellationToken);
+        if (booking == null)
+        {
+            return;
+        }
+
+        var user = await _userRepository.GetByIdAsync(booking.UserId, cancellationToken);
+        if (user == null)
+        {
+            return;
+        }
+
+        await _emailService.SendAsync(user.Email, "Subject", "Body");
+    }
+}
