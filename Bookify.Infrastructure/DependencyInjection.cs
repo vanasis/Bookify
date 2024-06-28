e6b0ed03b80b1222c36dcf53271cfd1f7c2557e0
Vanasis <Vanasisb@gmail.com>
2024-06-28 16:41:05 +0330

    Implement EmailService

--- Bookify.Infrastructure/DependencyInjection.cs
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Email;
 using Bookify.Infrastructure.Clock;
+using Bookify.Infrastructure.Email;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +12,8 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+
+        services.AddTransient<IEmailService, EmailService>();
 
         return services;
     }
